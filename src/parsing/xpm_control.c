180bbe413a25d11a5330e8c0bec9f93366c83821
Buket Çolak <bucolak@k1m07s01.42istanbul.com.tr>
2025-12-17 11:23:17 +0300

    xpm is corrected

--- src/parsing/xpm_control.c
+++ src/parsing/xpm_control.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   xpm_control.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: buket <buket@student.42.fr>                +#+  +:+       +#+        */
+/*   By: bucolak <bucolak@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/08 12:02:30 by bucolak           #+#    #+#             */
-/*   Updated: 2025/11/29 16:10:17 by buket            ###   ########.fr       */
+/*   Updated: 2025/12/17 11:21:53 by bucolak          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 static int	control_so(t_header *header)
 {
-	int	i;
-	int	end;
+	int		i;
+	int		end;
+	char	*old_so_path;
 
 	i = 0;
-	while (header->so_path[i])
+	while (header->so_path[i] && ft_isspace(header->so_path[i]) == 0)
 		i++;
 	end = i - 1;
+	old_so_path = header->so_path;
+	header->so_path = ft_strtrim(header->so_path, " \t\r\v\f");
+	free(old_so_path);
 	if (end < 3 || header->so_path[end] != 'm' || header->so_path[end
 		- 1] != 'p' || header->so_path[end - 2] != 'x' || header->so_path[end
 		- 3] != '.')
@@ -30,13 +34,17 @@ static int	control_so(t_header *header)
 
 static int	control_we(t_header *header)
 {
-	int	i;
-	int	end;
+	int		i;
+	int		end;
+	char	*old_we_path;
 
 	i = 0;
-	while (header->we_path[i])
+	while (header->we_path[i] && ft_isspace(header->we_path[i]) == 0)
 		i++;
 	end = i - 1;
+	old_we_path = header->we_path;
+	header->we_path = ft_strtrim(header->we_path, " \t\r\v\f");
+	free(old_we_path);
 	if (end < 3 || header->we_path[end] != 'm' || header->we_path[end
 		- 1] != 'p' || header->we_path[end - 2] != 'x' || header->we_path[end
 		- 3] != '.')
@@ -46,13 +54,17 @@ static int	control_we(t_header *header)
 
 static int	control_no(t_header *header)
 {
-	int	i;
-	int	end;
+	int		i;
+	int		end;
+	char	*old_no_path;
 
 	i = 0;
-	while (header->no_path[i])
+	while (header->no_path[i] && ft_isspace(header->no_path[i]) == 0)
 		i++;
 	end = i - 1;
+	old_no_path = header->no_path;
+	header->no_path = ft_strtrim(header->no_path, " \t\r\v\f");
+	free(old_no_path);
 	if (end < 3 || header->no_path[end] != 'm' || header->no_path[end
 		- 1] != 'p' || header->no_path[end - 2] != 'x' || header->no_path[end
 		- 3] != '.')
@@ -62,13 +74,17 @@ static int	control_no(t_header *header)
 
 static int	control_ea(t_header *header)
 {
-	int	i;
-	int	end;
+	int		i;
+	int		end;
+	char	*old_ea_path;
 
 	i = 0;
-	while (header->ea_path[i])
+	while (header->ea_path[i] && ft_isspace(header->ea_path[i]) == 0)
 		i++;
 	end = i - 1;
+	old_ea_path = header->ea_path;
+	header->ea_path = ft_strtrim(header->ea_path, " \t\r\v\f");
+	free(old_ea_path);
 	if (end < 3 || header->ea_path[end] != 'm' || header->ea_path[end
 		- 1] != 'p' || header->ea_path[end - 2] != 'x' || header->ea_path[end
 		- 3] != '.')
